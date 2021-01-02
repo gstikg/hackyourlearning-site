570ffb71ee5b1c06b804e459392264ef03e235af
PixelPoncho <ines.rosito@ucalgary.ca>
2021-01-02 01:42:50 -0700

    Removing in progress landing page banner content

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,14 @@
 // Importing Components from node_modules
 import React from 'react';
 
-// Importing Project-Defined Components
-import LandingBanner from '../components/LandingBanner';
-
 // Importing Styles
 import '../stylesheets/LandingPage.scss';
 
 const LandingPage = () => (
   <div className="landing-container">
-    <LandingBanner />
+    <div className="landing-banner--container">
+      Banner
+    </div>
 
     {/* Following content is for the About section */}
     <div className="landing-about--container">
