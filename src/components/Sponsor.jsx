22968c82567f911a45f64d770fc1902d836d9c71
longtv2222 <long.tv2222@gmail.com>
2021-01-14 00:46:31 -0700

    Logos done

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -6,30 +6,30 @@ import '../stylesheets/Sponsor.scss'
 
 const Sponsor = () => (
     <>
-        <div class="center">
-            <h3><b>Our 2021 Sponsor</b></h3>
+        <div class="center text">
+            <h3><b>Our Sponsors</b></h3>
         </div>
 
         <div class="zoom center">
             <a href="https://www.hsbc.ca/" target="_blank" rel="noopener noreferrer">
-                <img src={HSBC} alt="Schulich logo" width="500" height="300"></img>
+                <img src={HSBC} alt="Schulich logo" width="300" height="200"></img>
             </a>
         </div>
 
-        <div class="center">
+        <div class="center text">
             <h3><b>Our Partners</b></h3>
         </div>
         
         <div class="center">
             <div class="zoom imgContainer">
                 <a href="https://schulich.ucalgary.ca/" target="_blank" rel="noopener noreferrer">
-                    <img src={schulich} alt="Schulich logo" width="400" height="300"></img>
+                    <img src={schulich} alt="Schulich logo" width="300" height="230"></img>
                 </a>
             </div>
 
             <div class="zoom imgContainer">
                 <a href="https://www.facebook.com/zooengg/" target="_blank" rel="noopener noreferrer">
-                    <img src={zoo} alt="Schulich logo" width="400" height="300"></img>
+                    <img src={zoo} alt="Schulich logo" width="300" height="230"></img>
                 </a>
             </div>
         </div>
