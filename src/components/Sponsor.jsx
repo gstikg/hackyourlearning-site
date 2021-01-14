c4ed57a5e9fbf17c8c71094d895d4e5991729075
longtv2222 <long.tv2222@gmail.com>
2021-01-13 21:28:48 -0700

    Added image with website link

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import schulich from '../assets/schulich.png'   //Import schulich logo
+import HSBC from '../assets/HSBC.png'
+import zoo from '../assets/zoo.png'
 
 const Sponsor = () => (
     <>
-        <div classname="hello">
-            <p>
-                Hello
-            </p>
-        </div>
+        <p>Our 2021 Sponsor</p>
+
+        <a href="https://www.hsbc.ca/" target="_blank" rel="noopener noreferrer">
+            <img src={HSBC} alt="Schulich logo" width="500" height="300"></img>
+        </a>
+
+        <a href="https://schulich.ucalgary.ca/" target="_blank" rel="noopener noreferrer">
+            <img src={schulich} alt="Schulich logo" width="400" height="300"></img>
+        </a>
+
+        <a href="https://www.facebook.com/zooengg/" target="_blank" rel="noopener noreferrer">
+            <img src={zoo} alt="Schulich logo" width="400" height="300"></img>
+        </a>
+
+        <p>Our Partners</p>
+
     </>
 );
 
