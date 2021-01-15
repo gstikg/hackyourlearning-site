ca341a430389a591ecd89ffb3a54c7a741010606
longtv2222 <long.tv2222@gmail.com>
2021-01-14 17:38:26 -0700

    Added gradient to sponsor section

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -7,70 +7,71 @@ import HYLButton from '../components/HYLButton';
 
 const Sponsor = () => (
   <>
-    <div className="center text">
-      <h2><b>Meet Our Sponsors</b></h2>
-    </div>
+    <div id="gradient">
+      <div className="center text" >
+        <h2><b>Meet Our Sponsors</b></h2>
+      </div>
 
-    <div className="about-block--text center">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+      <div className="about-block--text center">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco.
       </p>
 
-      <p className="brtext">
-        Interested in sponsoring? Want more information? Email us at
+        <p className="brtext">
+          Interested in sponsoring? Want more information? Email us at
         {' '}
-        <b>
-          sponsor@hackyourlearning.ca
+          <b>
+            sponsor@hackyourlearning.ca
         </b>
-      </p>
-    </div>
+        </p>
+      </div>
 
-    <div className="button_position">
-      <HYLButton
-        color="blue"
-        width="400"
-        onClick={(e) => {
-          e.preventDefault();
-          window.open(
-            '', // Need to add website or form link here
-            '_blank',
-          );
-        }}
-      >
-        <b>Sponsor the Impact</b>
-      </HYLButton>
-    </div>
+      <div className="button_position">
+        <HYLButton
+          color="blue"
+          width="400"
+          onClick={(e) => {
+            e.preventDefault();
+            window.open(
+              '', // Need to add website or form link here
+              '_blank',
+            );
+          }}
+        >
+          <b>Sponsor the Impact</b>
+        </HYLButton>
+      </div>
 
-    <div className="center text">
-      <h3><b>Our Sponsors</b></h3>
-    </div>
+      <div className="center text">
+        <h3><b>Our Sponsors</b></h3>
+      </div>
 
-    <div className="zoom center">
-      <a href="https://www.hsbc.ca/" target="_blank" rel="noopener noreferrer">
-        <img src={HSBC} alt="Schulich logo" width="300" height="200" />
-      </a>
-    </div>
-
-    <div className="center text">
-      <h3><b>Our Partners</b></h3>
-    </div>
-
-    <div className="center">
-      <div className="zoom imgContainer">
-        <a href="https://schulich.ucalgary.ca/" target="_blank" rel="noopener noreferrer">
-          <img src={schulich} alt="Schulich logo" width="280" height="240" />
+      <div className="zoom center">
+        <a href="https://www.hsbc.ca/" target="_blank" rel="noopener noreferrer">
+          <img src={HSBC} alt="Schulich logo" width="300" height="200" />
         </a>
       </div>
 
-      <div className="zoom imgContainer">
-        <a href="https://www.facebook.com/zooengg/" target="_blank" rel="noopener noreferrer">
-          <img src={zoo} alt="Schulich logo" width="280" height="240" />
-        </a>
+      <div className="center text">
+        <h3><b>Our Partners</b></h3>
+      </div>
+
+      <div className="center">
+        <div className="zoom imgContainer">
+          <a href="https://schulich.ucalgary.ca/" target="_blank" rel="noopener noreferrer">
+            <img src={schulich} alt="Schulich logo" width="280" height="240" />
+          </a>
+        </div>
+
+        <div className="zoom imgContainer">
+          <a href="https://www.facebook.com/zooengg/" target="_blank" rel="noopener noreferrer">
+            <img src={zoo} alt="Schulich logo" width="280" height="240" />
+          </a>
+        </div>
       </div>
     </div>
-
   </>
 );
 
